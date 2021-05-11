6e93f9cb07e1d62fc52d0110528a979481c5705a
Víctor Galán <herreradelduque@gmail.com>
2021-05-11 14:38:42 +0200

    new try

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-model = load_model('deployment_tuned_gbr')
+model = load_model('deployment_28042020')
 
 def predict(model, input_df):
     predictions_df = predict_model(estimator=model, data=input_df)
@@ -13,37 +13,53 @@ def predict(model, input_df):
 def run():
 
     from PIL import Image
-    image_hospital = Image.open('hospital_insurance.jpg')
-    st.image(image_hospital)
+    image = Image.open('logo.png')
+    image_hospital = Image.open('hospital.jpg')
 
-    st.title("APP para la prediccion de los costes hospitalarios")
-    st.write('Esta es una palicacion para predecir los costes hospitalarios a traves\
-             de los datos de los pacientes')
+    st.image(image,use_column_width=False)
 
+    add_selectbox = st.sidebar.selectbox(
+    "How would you like to predict?",
+    ("Online", "Batch"))
 
-    age = st.sidebar.number_input('Age', min_value=1, max_value=100, value=25)
-    sex = st.sidebar.selectbox('Sex', ['male', 'female'])
-    bmi = st.sidebar.number_input('BMI', min_value=10, max_value=50, value=10)
-    children = st.sidebar.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
-    if st.sidebar.checkbox('Smoker'):
-        smoker = 'yes'
-    else:
-        smoker = 'no'
-    region = st.sidebar.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
+    st.sidebar.info('This app is created to predict patient hospital charges')
+    st.sidebar.success('https://www.pycaret.org')
+    
+    st.sidebar.image(image_hospital)
 
-    output=""
+    st.title("Insurance Charges Prediction App")
 
-    input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
-    input_df = pd.DataFrame([input_dict])
+    if add_selectbox == 'Online':
 
-    st.table(input_df)
+        age = st.number_input('Age', min_value=1, max_value=100, value=25)
+        sex = st.selectbox('Sex', ['male', 'female'])
+        bmi = st.number_input('BMI', min_value=10, max_value=50, value=10)
+        children = st.selectbox('Children', [0,1,2,3,4,5,6,7,8,9,10])
+        if st.checkbox('Smoker'):
+            smoker = 'yes'
+        else:
+            smoker = 'no'
+        region = st.selectbox('Region', ['southwest', 'northwest', 'northeast', 'southeast'])
 
-    if st.button("Predict"):
-        output = predict(model=model, input_df=input_df)
-        output = '$' + str(output)
+        output=""
 
-    st.success('The output is {}'.format(output))
+        input_dict = {'age' : age, 'sex' : sex, 'bmi' : bmi, 'children' : children, 'smoker' : smoker, 'region' : region}
+        input_df = pd.DataFrame([input_dict])
 
+        if st.button("Predict"):
+            output = predict(model=model, input_df=input_df)
+            output = '$' + str(output)
+
+        st.success('The output is {}'.format(output))
+
+    if add_selectbox == 'Batch':
+
+        file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+
+        if file_upload is not None:
+            data = pd.read_csv(file_upload)
+            predictions = predict_model(estimator=model,data=data)
+            st.write(predictions)
 
 if __name__ == '__main__':
     run()
